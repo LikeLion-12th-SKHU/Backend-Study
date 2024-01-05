3cfd2a32611d48312d3a4e6f147f8a9f9496662f
Rommmu <tonty1222@naver.com>
2024-01-06 00:45:42 +0900

    docs: Study chapter 2

--- rommmu/freelec-springboot2-webserver/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.jojoldo.book.freelecspringboot2webserver {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.jojoldo.book.freelecspringboot2webserver to javafx.fxml;
-    exports com.jojoldo.book.freelecspringboot2webserver;
-}
