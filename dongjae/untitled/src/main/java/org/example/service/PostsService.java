1d3b6a4e36adc9d2400ba92dd01ab069bc5e9bab
djdongjae <jk47wee@gmail.com>
2024-01-16 10:59:08 +0900

    feat: 게시글 저장

---
+++ dongjae/untitled/src/main/java/org/example/service/PostsService.java
@@ -0,0 +1,20 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.posts.PostsRepository;
+import org.example.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
