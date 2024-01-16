78a3bf865b7cc95371fdad704799586a34d1a0cc
djdongjae <jk47wee@gmail.com>
2024-01-16 14:12:28 +0900

    refactor: 200 -> 201

--- dongjae/untitled/src/main/java/org/example/web/PostsApiController.java
+++ dongjae/untitled/src/main/java/org/example/web/PostsApiController.java
@@ -1,10 +1,14 @@
 package org.example.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.common.dto.BaseResponse;
+import org.example.exception.SuccessCode;
 import org.example.service.PostsService;
 import org.example.web.dto.PostsSaveRequestDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,8 +18,10 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-        return postsService.save(requestDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public BaseResponse<Long> save(@RequestBody PostsSaveRequestDto requestDto) {
+        final Long data = postsService.save(requestDto);
+        return BaseResponse.success(SuccessCode.POST_SAVE_SUCCESS, data);
     }
 
 }
