18440ccfdb92ec48795b384d4f81edf641e40088
Rommmu <tonty1222@naver.com>
2024-01-26 00:00:17 +0900

    feat: form validation 우선순위 수정

--- rommmu/study-lsj/jpa1-project/src/main/java/com/rommmu/validation/ValidationSequence.java
+++ rommmu/study-lsj/jpa1-project/src/main/java/com/rommmu/validation/ValidationSequence.java
@@ -5,6 +5,6 @@ import jakarta.validation.GroupSequence;
 
 import static com.rommmu.validation.ValidationGroups.*;
 
-@GroupSequence({NotEmptyGroup.class, NotBlankGroup.class, PatternGroup.class, SizeGroup.class, MinGroup.class})
+@GroupSequence({NotEmptyGroup.class, NotBlankGroup.class, SizeGroup.class, MinGroup.class, PatternGroup.class})
 public interface ValidationSequence {
 }
